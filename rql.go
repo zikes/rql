9111e9760a6eb39320aee72263ec495e6c99c773
Jason Hutchinson <jhutchinson@wehco.com>
2017-02-21 11:01:14 -0600

    convenience functions for creating Expression types

--- rql.go
+++ rql.go
@@ -15,18 +15,113 @@ func ParseString(s string) (Statement, error) {
 	return NewParser(strings.NewReader(s)).Parse()
 }
 
+// Literals
+
+func Ident(s string) (Expression, error) {
+	if s == "" {
+		return nil, fmt.Errorf("Identifier must be non-empty")
+	}
+	return Identifier{
+		Kind: IDENT,
+		Name: s,
+	}, nil
+}
+func Lit(i interface{}) (Expression, error) {
+	switch i := i.(type) {
+	case int:
+		return Literal{Kind: NUMERIC, Value: strconv.Itoa(i)}, nil
+	case float64:
+		return Literal{Kind: NUMERIC, Value: strconv.FormatFloat(i, 'f', -1, 64)}, nil
+	case string:
+		return Literal{Kind: STRING, Value: i}, nil
+	case nil:
+		return Literal{Kind: NULL, Value: "null"}, nil
+	case bool:
+		s := "false"
+		if i {
+			s = "true"
+		}
+		return Literal{Kind: BOOLEAN, Value: s}, nil
+	default:
+		return nil, fmt.Errorf("unknown type")
+	}
+}
+
+// Operators
+
+func And(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     AND,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Or(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     OR,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Not(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     NOT,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Lt(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     LT,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Gt(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     GT,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Le(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     LE,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Ge(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     GE,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Eq(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     EQ,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func Ne(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     NE,
+		Operands: ExpressionList(e),
+	}, nil
+}
+func In(e ...Expression) (Expression, error) {
+	return Operator{
+		Kind:     IN,
+		Operands: ExpressionList(e),
+	}, nil
+}
+
+// Arrays
+
 func Array(e ...interface{}) (ExpressionList, error) {
 	out := ExpressionList{}
+
 	for _, v := range e {
+		lit, err := Lit(v)
+		if err == nil {
+			out = append(out, lit)
+			continue
+		}
 		switch v := v.(type) {
-		case nil:
-			out = append(out, Literal{Kind: NULL, Value: "null"})
-		case string:
-			out = append(out, Literal{Kind: STRING, Value: v})
-		case int:
-			out = append(out, Literal{Kind: NUMERIC, Value: strconv.Itoa(v)})
-		case float64:
-			out = append(out, Literal{Kind: NUMERIC, Value: strconv.FormatFloat(v, 'f', -1, 64)})
 		case Expression:
 			out = append(out, v)
 		default:
