06b9a989d958bbf2876215bcf6517829f63643d7
Jason Hutchinson <jhutchinson@wehco.com>
2017-02-22 17:32:50 -0600

    more tests

--- parser_test.go
+++ parser_test.go
@@ -76,6 +76,9 @@ func TestIntLiteral_Value(t *testing.T) {
 		{0, rql.Lit(0)},
 		{1, rql.Lit(1)},
 		{-1, rql.Lit(-1)},
+
+		// Overflows
+		{0, rql.Literal{rql.NUMERIC, "9223372036854775808"}},
 	}
 
 	fmt.Printf("Testing Integer Literal Valuation\n")
@@ -94,12 +97,15 @@ func TestFloatLiteral_Value(t *testing.T) {
 		{float64(0), rql.Lit(float64(0))},
 		{1.1, rql.Lit(1.1)},
 		{-1.1, rql.Lit(-1.1)},
+		{float64(9.223372036854776e+26), rql.Literal{rql.NUMERIC, "922337203685477580812345968"}},
+
+		{0, rql.Literal{rql.NUMERIC, "abc"}},
 	}
 
 	fmt.Printf("Testing Float Literal Valuation\n")
 	for i, tt := range tests {
-		if tt.lit.FloatValue() != tt.exp {
-			t.Errorf("  %d %q\n\nmismatch:\n    exp=%s\n    got=%s\n\n", i, tt.lit, tt.exp, tt.lit.IntValue())
+		if !reflect.DeepEqual(tt.lit.FloatValue(), tt.exp) {
+			t.Errorf("  %d %q\n\nmismatch:\n    exp=%s\n    got=%s\n\n", i, tt.lit, tt.exp, tt.lit.FloatValue())
 		}
 	}
 }
@@ -157,6 +163,7 @@ func TestExpressions_String(t *testing.T) {
 		// Literal Booleans
 		{"true", rql.Literal{rql.BOOLEAN, "true"}},
 		{"false", rql.Literal{rql.BOOLEAN, "false"}},
+		{"null", rql.Literal{rql.NULL, "null"}},
 
 		// Identifiers
 		{"col", rql.Identifier{Kind: rql.IDENT, Name: "col"}},
@@ -196,9 +203,36 @@ func TestExpressions_String(t *testing.T) {
 			rql.Identifier{Kind: rql.IDENT, Name: "id1"},
 			rql.Identifier{Kind: rql.IDENT, Name: "id2"},
 		}},
+		{"(id1,id2)", rql.ExpressionList{
+			rql.Identifier{Kind: rql.IDENT, Name: "id1"},
+			rql.Whitespace{Kind: rql.WS, Value: " "},
+			rql.Identifier{Kind: rql.IDENT, Name: "id2"},
+		}},
+
+		// Whitespace
+		{" ", rql.Whitespace{Kind: rql.WS, Value: " "}},
 	}
 
 	fmt.Printf("Testing Expression.String()\n")
+
+	for i, tt := range tests {
+		got := tt.e.String()
+		if tt.s != got {
+			t.Errorf("  %d. string mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, got)
+		}
+	}
+}
+
+func TestStatement_String(t *testing.T) {
+	var tests = []struct {
+		s string
+		e rql.Statement
+	}{
+		// Statement
+		{"eq()", rql.Statement{rql.Operator{Kind: rql.EQ}, rql.Whitespace{rql.WS, " "}}},
+	}
+
+	fmt.Printf("Testing Statement.String()\n")
 
 	for i, tt := range tests {
 		got := tt.e.String()
@@ -284,6 +318,149 @@ func TestParser_ParseStatement(t *testing.T) {
 			t.Errorf("  %d. %q: error mismatch:\n    exp=%s\n    got=%s\n\n", i, tt.s, tt.err, err)
 		} else if tt.err == "" && !reflect.DeepEqual(tt.stmt, stmt) {
 			t.Errorf("  %d. %q\n\nstmt mismatch:\n    exp=%s\n    got=%s\n\n", i, tt.s, tt.stmt, stmt)
+		}
+	}
+}
+
+func TestInvalidSyntax(t *testing.T) {
+	var tests = []struct {
+		s   string
+		exp error
+	}{
+		{``, fmt.Errorf("found EOF, expected operator")},
+		{`and(*)`, fmt.Errorf("error parsing expression list: found *, expected value expression")},
+		{`or`, fmt.Errorf("error parsing expression list: found EOF, expected open parentheses")},
+		{`or)`, fmt.Errorf("error parsing expression list: found ), expected open parentheses")},
+		{`or{`, fmt.Errorf("error parsing expression list: found {, expected open parentheses")},
+		{`or("unclosed string)`, fmt.Errorf("error parsing expression list: found EOF, expected comma or close parentheses")},
+	}
+
+	fmt.Printf("Testing Errors\n")
+	for i, tt := range tests {
+		stmt, err := rql.ParseString(tt.s)
+		if !reflect.DeepEqual(tt.exp, err) {
+			t.Errorf("  %d. %v - %v", i, stmt, err)
+		}
+	}
+}
+
+func TestExpressionTokenValues(t *testing.T) {
+	var tests = []struct {
+		e rql.Expression
+		t rql.Token
+	}{
+		// Arrays
+		{rql.Array(), rql.EXPR_LIST},
+
+		// Operators
+		{rql.And(), rql.AND},
+		{rql.Or(), rql.OR},
+		{rql.Not(), rql.NOT},
+		{rql.Lt(), rql.LT},
+		{rql.Gt(), rql.GT},
+		{rql.Le(), rql.LE},
+		{rql.Ge(), rql.GE},
+		{rql.Eq(), rql.EQ},
+		{rql.Ne(), rql.NE},
+		{rql.In(), rql.IN},
+
+		// Literals
+		{rql.Lit(""), rql.STRING},
+		{rql.Lit(1), rql.NUMERIC},
+		{rql.Lit(1.23), rql.NUMERIC},
+		{rql.Lit(nil), rql.NULL},
+
+		// Punctuation
+		{rql.Punctuation{rql.COMMA, ","}, rql.COMMA},
+		{rql.Punctuation{rql.LPAREN, "("}, rql.LPAREN},
+		{rql.Punctuation{rql.RPAREN, ")"}, rql.RPAREN},
+
+		// Whitespace
+		{rql.Whitespace{rql.WS, " "}, rql.WS},
+	}
+
+	fmt.Printf("Testing Expression Token Values\n")
+	for i, tt := range tests {
+		testToken := tt.e.Token()
+		if !reflect.DeepEqual(testToken, tt.t) {
+			t.Errorf("  %d. token mismatch:\n   exp=%s\n   got=%s\n", i, tt.t, testToken)
+		}
+	}
+}
+
+func TestTokens(t *testing.T) {
+	var tests = []struct {
+		token         rql.Token
+		str           string
+		isLiteral     bool
+		isOperator    bool
+		isPunctuation bool
+		isArray       bool
+		isWhitespace  bool
+		isEOF         bool
+		isIllegal     bool
+		isIdentifier  bool
+		isValue       bool
+	}{
+		// token,       str,         lit,   op,    punct, arr,   ws,    eof,   ill,   ident, val
+		{rql.ILLEGAL, `ILLEGAL`, false, false, false, false, false, false, true, false, false},
+		{rql.EOF, `EOF`, false, false, false, false, false, true, false, false, false},
+		{rql.WS, `WS`, false, false, false, false, true, false, false, false, false},
+
+		{rql.IDENT, `IDENT`, true, false, false, false, false, false, false, true, true},
+		{rql.NUMERIC, `NUMERIC`, true, false, false, false, false, false, false, false, true},
+		{rql.STRING, `STRING`, true, false, false, false, false, false, false, false, true},
+		{rql.BOOLEAN, `BOOLEAN`, true, false, false, false, false, false, false, false, true},
+		{rql.NULL, `NULL`, true, false, false, false, false, false, false, false, true},
+
+		{rql.AND, `and`, false, true, false, false, false, false, false, false, true},
+		{rql.OR, `or`, false, true, false, false, false, false, false, false, true},
+		{rql.NOT, `not`, false, true, false, false, false, false, false, false, true},
+		{rql.LT, `lt`, false, true, false, false, false, false, false, false, true},
+		{rql.GT, `gt`, false, true, false, false, false, false, false, false, true},
+		{rql.LE, `le`, false, true, false, false, false, false, false, false, true},
+		{rql.GE, `ge`, false, true, false, false, false, false, false, false, true},
+		{rql.EQ, `eq`, false, true, false, false, false, false, false, false, true},
+		{rql.NE, `ne`, false, true, false, false, false, false, false, false, true},
+		{rql.IN, `in`, false, true, false, false, false, false, false, false, true},
+
+		{rql.EXPR_LIST, `EXPR_LIST`, false, false, false, true, false, false, false, false, true},
+		{rql.Token(12345), `token(12345)`, false, false, false, false, false, false, true, false, false},
+	}
+
+	fmt.Println("Testing Tokens")
+
+	for i, tt := range tests {
+		tok := tt.token
+		if tok.String() != tt.str {
+			t.Errorf("  %d. string mismatch:\n   exp=%s\n   got=%s\n", i, tt.str, tok.String())
+		}
+		if tok.IsLiteral() != tt.isLiteral {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isLiteral, tok.IsLiteral())
+		}
+		if tok.IsOperator() != tt.isOperator {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isOperator, tok.IsOperator())
+		}
+		if tok.IsPunctuation() != tt.isPunctuation {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isPunctuation, tok.IsPunctuation())
+		}
+		if tok.IsEOF() != tt.isEOF {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isEOF, tok.IsEOF())
+		}
+		if tok.IsIllegal() != tt.isIllegal {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isIllegal, tok.IsIllegal())
+		}
+		if tok.IsIdentifier() != tt.isIdentifier {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isIdentifier, tok.IsIdentifier())
+		}
+		if tok.IsArray() != tt.isArray {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isArray, tok.IsArray())
+		}
+		if tok.IsWhitespace() != tt.isWhitespace {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isWhitespace, tok.IsWhitespace())
+		}
+		if tok.IsValue() != tt.isValue {
+			t.Errorf("  %d. type mismatch:\n   exp=%s\n   got=%s\n", i, tt.isValue, tok.IsValue())
 		}
 	}
 }
