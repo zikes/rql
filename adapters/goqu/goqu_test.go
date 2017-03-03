9c9f873470515a6c514e60e19401c6d8cb1df5ee
Jason Hutchinson <jhutchinson@wehco.com>
2017-03-03 11:05:16 -0600

    add goqu conversion tests

---
+++ adapters/goqu/goqu_test.go
@@ -0,0 +1,42 @@
+package goquadapter
+
+import (
+	"testing"
+
+	rql "git.nwaonline.com/rune/rql/parse"
+)
+
+type parseTest struct {
+	name   string
+	input  string
+	result string
+}
+
+var parseTests = []parseTest{
+	{"empty", "", `SELECT * FROM "test"`},
+	{"nested", "and(eq(id,12),or(lt(age,21),gt(height,156.2)))", `SELECT * FROM "test" WHERE (("id" = 12) AND (("age" < 21) OR ("height" > 156.2)))`},
+
+	// operators
+	{"equals", "eq(id,12)", `SELECT * FROM "test" WHERE ("id" = 12)`},
+	{"not equals", "ne(id,12)", `SELECT * FROM "test" WHERE ("id" != 12)`},
+	{"less than", "lt(id,12)", `SELECT * FROM "test" WHERE ("id" < 12)`},
+	{"greater than", "gt(id,12)", `SELECT * FROM "test" WHERE ("id" > 12)`},
+	{"less than equals", "le(id,12)", `SELECT * FROM "test" WHERE ("id" <= 12)`},
+	{"greater than equals", "ge(id,12)", `SELECT * FROM "test" WHERE ("id" >= 12)`},
+	{"and", "and(eq(id,12),lt(age,21))", `SELECT * FROM "test" WHERE (("id" = 12) AND ("age" < 21))`},
+	{"or", "or(eq(id,12),lt(age,21))", `SELECT * FROM "test" WHERE (("id" = 12) OR ("age" < 21))`},
+	{"in", "in(id,(12,13,14))", `SELECT * FROM "test" WHERE ("id" IN ((12, 13, 14)))`},
+}
+
+func TestParse(t *testing.T) {
+	for _, test := range parseTests {
+		stmt, err := rql.New(test.name).Parse(test.input)
+		if err != nil {
+			t.Fatalf("unexpected parse failure: %v", err)
+		}
+		got := ToSQL(stmt.Root)
+		if got != test.result {
+			t.Errorf("%s: SQL mismatch\n\texpected:\n\t\t%s\n\tgot:\n\t\t%s", test.name, test.result, got)
+		}
+	}
+}
